4970a67e5d81bf01b99723573575d4533a131901
danilo90lan <15630@coderacademy.edu.au>
2024-12-04 20:01:30 +1100

    aded form submission and rendering API results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,57 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function Home() {
-    const [query, setQuery] = useState("");
+    const [query, setQuery] = useState([]);
+    const [movies, setMovies] = useState([]);
 
     const fetchMovies = async () => {
         // using fetch
-        const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`);
+        // const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`);
+
+        //using AXIOS
+        if (query) {
+            const response = await axios.get("http://www.omdbapi.com/", {
+                params: {
+                    apikey: "93de2e78",
+                    s: query.trim()
+                }
+            });
+            setMovies(response.data.Search);
+        }
     }
+
 
     return (
         <div>
             <h1>Movie Search</h1>
-            <input
-                type="text"
-                placeholder="Search for Movies...."
-            />
-            <button>Search</button>
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                fetchMovies()
+            }
+            }
+            >
+                <input
+                    type="text"
+                    placeholder="Search for Movies...."
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                    required />
+
+                <button type="submit">Search</button>
+            </form>
+
+
+            <ul>
+                {movies.map((movie, index) => {
+                    return <li key={index}>
+                        <p>{movie.Title}</p>
+                        <p>{movie.Year}</p>
+                        <img src={movie.Poster} />
+                    </li>
+                })
+                }
+            </ul>
         </div>
     )
 }
