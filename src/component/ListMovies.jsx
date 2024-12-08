12ccf536c04ae9fe5bc93e3e3fffa2f7775baeee
danilo90lan <15630@coderacademy.edu.au>
2024-12-08 11:28:13 +1100

    watchlist validation, button enabled or disabled based on watchlist state

--- src/component/ListMovies.jsx
+++ src/component/ListMovies.jsx
@@ -3,16 +3,27 @@ import { useWatchlist } from "../context/WatchListContext";
 
 export default function ListMovies(props) {
     const { movie } = props;
-    const { addToWatchlist } = useWatchlist();
+    const { watchlist, addToWatchlist } = useWatchlist();
+
+    // it returns true or false if the item is found
+    const isInWatchlist = watchlist.some((item) => item.imdbID === movie.imdbID);
 
     return (
-        <div className="col-md-4 movie-card" style={{maxWidth: "18rem"}}>
+        <div className="col-md-4 movie-card" style={{ maxWidth: "18rem" }}>
             <h3>{movie.Title}</h3>
             <p>{movie.Year}</p>
             <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-            <button onClick={
-                () => addToWatchlist(movie)}> Add to WatchList</button>
-            <Link to = {`/details/${movie.imdbID}`} className="btn btn-primary">View Details</Link>
+            <button
+                // it's a javascript statement incorporated in htm/css tag attribute class name
+                className={`${isInWatchlist ? "btn btn-success" : "btn btn-primary"}`}
+                onClick={() => addToWatchlist(movie)}
+                disabled={isInWatchlist}
+                > 
+                {/* visualize this string button based on the value */}
+                {`${isInWatchlist ? "Added in watchlist" : "Add to Watchlist"}`}</button>
+                
+
+            <Link to={`/details/${movie.imdbID}`} className="btn btn-primary">View Details</Link>
         </div>
 
 
--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useWatchlist } from "../context/WatchListContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Watchlist() {
     const { watchlist, removeFromWatchlist } = useWatchlist();
+    const navigate = useNavigate();
 
     return (
         <div>
+            <button className="btn btn-secondary mb-3" onClick={() => navigate(-1)}>Go back</button>
             <h2>Your Watch List</h2>
             {watchlist.length === 0 ? (
                 <p>Your watchlist is empty. Add some movies.</p>
